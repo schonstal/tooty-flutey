3a758e2e7714d64fc8997eebcebe6a328e0a03b5
Josh Schonstal <josh@schonstal.org>
2016-01-31 10:54:14 -0800

    Select devices on startup

--- Assets/Source/RecorderInput.cs
+++ Assets/Source/RecorderInput.cs
@@ -6,10 +6,18 @@ public class RecorderInput : MonoBehaviour {
   public static int SAMPLES = 8192;
   public static int SAMPLE_RATE = 44100;
 
-  public string deviceName = "Princess";
   public float highNote = 1661.22f;
   public float lowNote = 1244.51f;
   public float tolerance = 100f;
+
+  public bool IsInitialized {
+    get {
+      return initialized;
+    }
+  }
+
+  bool initialized = false;
+  string deviceName = "Princess";
 
   float[] spectrum = new float[SAMPLES];
   AudioSource audioSource;
@@ -26,6 +34,7 @@ public class RecorderInput : MonoBehaviour {
   }
 
   void Update() {
+    if (!initialized) return;
     frequency = GetFrequency();
   }
 
@@ -56,9 +65,8 @@ public class RecorderInput : MonoBehaviour {
     return noteTriggered(lowNote);
   }
 
-  void OnGUI() {
-    GUI.Label(new Rect(0,0, Screen.width, Screen.height), "Frequency: " + frequency + "Hz");
-    GUI.Label(new Rect(0,20, Screen.width, Screen.height), "High: " + highTriggered());
-    GUI.Label(new Rect(0,40, Screen.width, Screen.height), "Low: " + lowTriggered());
+  public void initialize(string device) {
+    initialized = true;
+    deviceName = device;
   }
 }
--- Assets/Source/RecorderTilt.cs
+++ Assets/Source/RecorderTilt.cs
@@ -14,6 +14,22 @@ public class RecorderTilt : MonoBehaviour {
   }
 
   void Update() {
+    if (!xRecorder.IsInitialized) {
+      for (int i = 0; i < Microphone.devices.Length - 1; i++) {
+        if (Input.GetKeyDown(i.ToString())) {
+          xRecorder.initialize(Microphone.devices[i]);
+        }
+      }
+      return;
+    } else if (!zRecorder.IsInitialized) {
+      for (int i = 0; i < Microphone.devices.Length - 1; i++) {
+        if (Input.GetKeyDown(i.ToString())) {
+          zRecorder.initialize(Microphone.devices[i]);
+        }
+      }
+      return;
+    }
+
     var xRotation = 0;
     if (xRecorder.lowTriggered()) {
       xRotation = 10;
@@ -29,5 +45,21 @@ public class RecorderTilt : MonoBehaviour {
     }
     
     transform.eulerAngles = new Vector3(xRotation, 0, zRotation);
+  }
+
+  void OnGUI() {
+    if (!xRecorder.IsInitialized) {
+      GUI.Label(new Rect(0,0, Screen.width, Screen.height), "Select X Device (press key)");
+      for (int i = 0; i < Microphone.devices.Length - 1; i++) {
+        GUI.Label(new Rect(0,20 * (i+1), Screen.width, Screen.height), string.Format("[{0}] {1}", i, Microphone.devices[i]));
+      }
+      return;
+    } else if (!zRecorder.IsInitialized) {
+      GUI.Label(new Rect(0,0, Screen.width, Screen.height), "Select Z Device (press key)");
+      for (int i = 0; i < Microphone.devices.Length - 1; i++) {
+        GUI.Label(new Rect(0,20 * (i+1), Screen.width, Screen.height), string.Format("[{0}] {1}", i, Microphone.devices[i]));
+      }
+      return;
+    }
   }
 }
