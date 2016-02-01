e3dbe66b249ef996c8c3bd2430369d1679e03586
Josh Schonstal <josh@schonstal.org>
2016-01-31 17:11:30 -0800

    Game logic

---
+++ Assets/Source/Goal.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+  void Start() {
+  }
+
+  void Update() {
+    transform.Rotate(new Vector3(1.5f, 1, 0.5f));
+  }
+}
---
+++ Assets/Source/TweenIn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TweenIn : MonoBehaviour {
+  void Start() {
+    transform.position = new Vector3(
+      transform.position.x,
+      -4,
+      transform.position.z
+    );
+
+    transform.localScale = new Vector3(0,0,0);
+
+    float tweenTime = 1f + Random.value * 0.5f;
+
+    iTween.MoveTo(gameObject, iTween.Hash(
+      "y", 0,
+      "time", tweenTime,
+      "easetype", "easeOutElastic"
+    ));
+
+    iTween.ScaleTo(gameObject, iTween.Hash(
+      "x", 4,
+      "y", 4,
+      "z", 0.5,
+      "time", tweenTime,
+      "easetype", "easeOutElastic"
+    ));
+  }
+}
